01a7ed2d97ee07b89defc735e5fff80713f74927
Melon-ali <you@example.com>
2023-11-22 12:48:27 +0600

    e-shop all product added

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,16 +1,17 @@
 import Container from "@/app/components/Container"
-import { product } from "@/utils/product"
 import ProductDetails from "./ProductDetails"
 import ListRating from "./ListRating"
+import { products } from "@/utils/products"
 
 interface IParams {
     productId : any
 }
 
 const Product = ({ params}: {params: IParams}) => {
-    console.log("params", params)
+  console.log("params", params)
 
-    product
+  const product = products.find((item) => item.id === params.productId);
+
   return (
     <div className="p-8">
         <Container>
