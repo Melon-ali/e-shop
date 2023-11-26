6235914a79907d7f61cad216b74e6249edd35062
Melon-ali <you@example.com>
2023-11-26 10:51:43 +0600

    user created e-shop

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,13 +4,15 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request){
     const body = await request.json()
-    const {name, email, password} = body
+    const {name, email, password} = body;
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await prisma.user.create({
         data: {
             name, email, hashedPassword
-        }
-    })
+        },
+    });
+
+    return NextResponse.json(user);
 }
