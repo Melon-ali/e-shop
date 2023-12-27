74fd15e81924211a631c32e7a96c8451da7e1088
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2023-12-28 02:32:10 +0600

    project update

--- actions/getCurrentUser.ts
+++ actions/getCurrentUser.ts
@@ -1,39 +1,37 @@
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
-import prisma from '@/libs/prismadb';
+import prisma from "@/libs/prismadb";
 
-export const getSession = async() => {
-    return await getServerSession(authOptions)
+export async function getSession() {
+  return await getServerSession(authOptions);
 }
 
-export default async function getCurrentUser() {
-    try {
-        const session = await getSession()
+export const getCurrentUser = async () => {
+  try {
+    const session = await getSession();
 
-        if(!session?.user?.email){
-            return null
-        }
-
-        const currentUser = await prisma.user.findUnique({
-            where:{
-                email: session?.user?.email
-            },
-            include: {orders: true},
-        });
-
-        if(!currentUser){
-            return null
-        }
-
-        return{
-            ...currentUser,
-            createdAt: currentUser.createAt.toISOString(),
-            updateAt: currentUser.updateAt.toISOString(),
-            emailVerified: currentUser.emailVerified?.toString() || null
-
-        }
-
-    } catch (error:any) {
-        return null
+    if (!session?.user?.email) {
+      return null;
     }
-}
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session?.user?.email,
+      },
+      include: { orders: true },
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return {
+      ...currentUser,
+      createdAt: currentUser.createAt.toISOString(),
+      updateAt: currentUser.updateAt.toISOString(),
+      emailVerified: currentUser.emailVerified?.toString() || null,
+    };
+  } catch (error: any) {
+    return null;
+  }
+};
--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -2,25 +2,23 @@ import Container from "@/app/components/Container";
 import FormWrap from "@/app/components/FormWrap";
 import AddProductForm from "./AddProductForm";
 import NullData from "@/app/components/NullData";
-import getCurrentUser from "@/actions/getCurrentUser";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
+const AddProducts = async () => {
+  const currentUser = await getCurrentUser();
 
-const AddProducts = async() => {
-
-    const currentUser = await getCurrentUser()
-
-    if(!currentUser || currentUser.role !== 'ADMIN'){
-        return <NullData title="Ooops! Access denied"/>
-    }
-    return (
-        <div>
-            <Container>
-                <FormWrap>
-                    <AddProductForm />
-                </FormWrap>
-            </Container>
-        </div>
-    );
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Ooops! Access denied" />;
+  }
+  return (
+    <div>
+      <Container>
+        <FormWrap>
+          <AddProductForm />
+        </FormWrap>
+      </Container>
+    </div>
+  );
 };
 
 export default AddProducts;
