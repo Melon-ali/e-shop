5161d823f92dbaa626c80a875bcad878fa5b3044
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2023-12-28 18:08:45 +0600

    ecommerce update

--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -2,7 +2,7 @@ import Container from "@/app/components/Container";
 import FormWrap from "@/app/components/FormWrap";
 import AddProductForm from "./AddProductForm";
 import NullData from "@/app/components/NullData";
-import { getCurrentUser } from "@/actions/getCurrentUser";
+import getCurrentUser from "@/actions/getCurrentUser";
 
 const AddProducts = async () => {
   const currentUser = await getCurrentUser();
